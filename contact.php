aa00bf01e7d28257bdfc5d8128f16ac42eee7516
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-02-01 17:18:12 +1100

    php

--- contact.php
+++ contact.php
@@ -1,11 +1,13 @@
 <?php
      
     $first_name = $_POST['name']; // required
+    $test = $_POST['name1']; 
     $email_from = $_POST['txtEmail']; // required
     $telephone = $_POST['txtPhone']; // not required
     $comments = $_POST['txtMsg']; // required
     echo "$first_name";
     echo "$email_from";
     echo "$telephone";
+    echo "$test";
 
   ?>
